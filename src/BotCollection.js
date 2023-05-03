b15fc608d72a20e56f02afdff4282e6c6fd5ce81
unknown <sirkaranja12@gmail.com>
2023-05-03 07:07:51 -0700

    fetching of the bots

--- src/BotCollection.js
+++ src/BotCollection.js
@@ -11,22 +11,7 @@ function BotCollection(props) {
   }, []);
 
   return (
-    <div>
-      <h1>Bot Collection</h1>
-      <div className="card-container">
-        {bots.map(bot => (
-          <div key={bot.id} className="card" onClick={() => props.onEnlistBot(bot)}>
-            <img src={bot.avatar_url} alt={bot.name} />
-            <div className="card-content">
-              <h2>{bot.name}</h2>
-              <p>Health: {bot.health}</p>
-              <p>Damage: {bot.damage}</p>
-              <p>Armor: {bot.armor}</p>
-              <p>Class: {bot.bot_class}</p>
-              <p>Catchphrase: {bot.catchphrase}</p>
-            </div>
-          </div>
-        ))}
+  
       </div>
     </div>
   );
